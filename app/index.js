0dc4baf0f6e1790039cb89c9125f4510b0424f38
dheerajtp <dheerajtp6338@gmail.com>
2024-02-05 21:52:50 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,37 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import styles from "../assets/styles";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+const App = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    "Roboto-Black": require("../assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Bold": require("../assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Light": require("../assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Medium": require("../assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <StatusBar style="auto" />
+      <Text style={styles.text}>Index file</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,11 @@
+## EV Charging Station Finder
+
+1. Created project using expo
+
+2. Setup expo router for routing [Install Expo Router](https://docs.expo.dev/router/installation/#manual-installation)
+
+3. Custom App Font
+
+We can integrate custom font to our expo application using [Expo font](https://docs.expo.dev/versions/latest/sdk/font/)
+
+4. [Expo splash Screen](https://docs.expo.dev/versions/latest/sdk/splash-screen/)
