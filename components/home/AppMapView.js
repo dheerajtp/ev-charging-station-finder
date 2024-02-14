3d481553674d4b6c553c84b8b8a6860ceafe3579
dheerajtp <dheerajtp6338@gmail.com>
2024-02-14 07:05:12 +0530

    :sparkles: Added get user  location for the map

--- components/home/AppMapView.js
+++ components/home/AppMapView.js
@@ -2,8 +2,28 @@ import { View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import styles from "../../assets/styles";
 import customMapStyle from "../../utils/MapViewStyle.json";
+import * as Location from "expo-location";
+import { useState, useEffect } from "react";
 
 const AppMapView = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  console.log(location, "location");
+
   return (
     <View>
       <MapView
