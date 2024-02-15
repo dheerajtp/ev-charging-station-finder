724cc16b6abc7339acf770906eefe2d9a2c1c500
dheerajtp <dheerajtp6338@gmail.com>
2024-02-15 22:25:58 +0530

    :sparkles: Search Bar and google places API Integration

--- app/home/(tabs)/home.js
+++ app/home/(tabs)/home.js
@@ -2,12 +2,14 @@ import { Text, View } from "react-native";
 import AppMapView from "../../../components/home/AppMapView";
 import styles from "../../../assets/styles";
 import Header from "../../../components/home/Header";
+import SearchBar from "../../../components/home/SearchBar";
 
 const Home = () => {
   return (
     <View style={styles.mapContainer}>
       <View style={styles.headerContainer}>
         <Header />
+        <SearchBar searchedLocation={(location) => console.log(location)} />
       </View>
       <AppMapView />
     </View>
